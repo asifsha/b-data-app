5d448615e14c63f0ba2b233069d8dbab34d4df1d
asifsha <asif-sharif@hotmail.com>
2019-04-24 00:33:09 +0500

    Chart added

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -6,106 +6,231 @@ import { connect } from 'react-redux'
 import * as apiActions from '../../actions/apiActions';
 
 import {
-    PieChart, Pie, Sector, Cell,
-    RadialBarChart, RadialBar, Legend
-  } from 'recharts';
+  PieChart, Pie, Sector, Cell,
+  RadialBarChart, RadialBar, Legend,
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip,
+  BarChart, Bar,
+  ScatterChart, Scatter, ZAxis,
+} from 'recharts';
+
+
+import Tree from 'react-d3-tree';
 
 
 class ChartsExport extends Component {
 
-    constructor(props, context) {
-        super(props, context);
-               
+  constructor(props, context) {
+    super(props, context);
 
+
+  }
+
+  componentDidMount() {
+    this.props.actions.GetData();
+  }
+
+  customizedTick(props) {
+    const { x, y, index, payload, width } = props;
+    //console.log(props, payload);
+    if (payload.value !== 'ETH') {
+      return (
+        <g>
+          <line x1={x - 1.5 * width} y1={y} x2={x - 1.5 * width} y2={y + 10} />
+          <text x={x} y={y} textAnchor="middle" dominantBaseline="hanging">
+            {payload.value}
+          </text>
+        </g>
+      );
     }
+  }
 
-    componentDidMount(){
-        this.props.actions.GetData();
-    }
-  
+  getsqrt(value) {
+    return Math.sqrt(value);
+  }
 
-    render() {       
- console.log(this.props.data);
-        const data01 = [
-            { name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
-            { name: 'Group C', value: 300 }, { name: 'Group D', value: 200 },
-          ];
-          const data02 = [
-            { name: 'A1', value: 100 },
-            { name: 'A2', value: 300 },
-            { name: 'B1', value: 100 },
-            { name: 'B2', value: 80 },
-            { name: 'B3', value: 40 },
-            { name: 'B4', value: 30 },
-            { name: 'B5', value: 50 },
-            { name: 'C1', value: 100 },
-            { name: 'C2', value: 200 },
-            { name: 'D1', value: 150 },
-            { name: 'D2', value: 50 },
-          ];
 
-          const data = [
-            {
-              name: '18-24', uv: 31.47, pv: 2400, fill: '#8884d8',
-            },
-            {
-              name: '25-29', uv: 26.69, pv: 4567, fill: '#83a6ed',
-            },
-            {
-              name: '30-34', uv: 15.69, pv: 1398, fill: '#8dd1e1',
-            },
-            {
-              name: '35-39', uv: 8.22, pv: 9800, fill: '#82ca9d',
-            },
-            {
-              name: '40-49', uv: 8.63, pv: 3908, fill: '#a4de6c',
-            },
-            {
-              name: '50+', uv: 2.63, pv: 4800, fill: '#d0ed57',
-            },
-            {
-              name: 'unknow', uv: 6.67, pv: 4800, fill: '#ffc658',
-            },
-          ];
-          
-          const style = {
-            top: 0,
-            left: 350,
-            lineHeight: '24px',
-          };
-        return (
-            <div>
-            <PieChart width={400} height={400}>
-        <Pie data={data01} dataKey="value" cx={200} cy={200} outerRadius={60} fill="#8884d8" />
-        <Pie data={data02} dataKey="value" cx={200} cy={200} innerRadius={70} outerRadius={90} fill="#82ca9d" label />
-      </PieChart>
-       <RadialBarChart width={500} height={300} cx={150} cy={150} innerRadius={20} outerRadius={140} barSize={10} data={data}>
-       <RadialBar minAngle={15} label={{ position: 'insideStart', fill: '#fff' }} background clockWise dataKey="uv" />
-       <Legend iconSize={10} width={120} height={140} layout="vertical" verticalAlign="middle" wrapperStyle={style} />
-     </RadialBarChart>
-     </div>
-        );
-    }
+
+
+  render() {
+    console.log(this.props.data);
+    
+
+    return (
+      <div >
+
+        <div id="treeWrapper"  style={{ width: '500px', height: '500px' }}>
+
+          <Tree data={this.props.data} orientation="vertical" />
+
+        </div>
+      </div>
+    );
+  }
 }
 
 // ItemDetails.propTypes = {
 //     items: PropTypes.array
 // };
 
+function groupBy(list, keyGetter) {
+  const map = new Map();
+  list.forEach((item) => {
+    const key = keyGetter(item);
+    const collection = map.get(key);
+    if (!collection) {
+      map.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+  });
+  return map;
+}
 
 
 function mapStateToProps(state, ownProps) {
-    // const itemId = ownProps.match.params.id; // fomr the path course/:id
-    // let item = { id: '', name: '', date: '', price: 0, inStock: false, type: '' };
+  // const itemId = ownProps.match.params.id; // fomr the path course/:id
+  // let item = { id: '', name: '', date: '', price: 0, inStock: false, type: '' };
 
-    // if (itemId && state.items.length > 0) {
-    //     item = getItemById(state.items, itemId);
-    // }
-    console.log('mapstatetoprops');
-    console.log(state);
-    return {
-        data: state.data        
+  // if (itemId && state.items.length > 0) {
+  //     item = getItemById(state.items, itemId);
+  // }
+  console.log('mapstatetoprops');
+  //console.log(state);
+
+  let treeData=[
+    {
+      name: 'Top Level',
+      attributes: {
+        keyA: 'val A',
+        keyB: 'val B',
+        keyC: 'val C',
+      },
+      children: [
+        {
+          name: 'Level 2: A',
+          attributes: {
+            keyA: 'val A',
+            keyB: 'val B',
+            keyC: 'val C',
+          },
+        },
+        {
+          name: 'Level 2: B',
+        },
+      ],
+    },
+  ];
+  let d = state.data;
+  let totalRecords = 0;
+  let icoRecords = 0;
+  let preIcoRecords = 0;
+  let finalIcoRecords = 0;
+  let preIcoGrouped, icoGrouped, finalIcoGrouped;
+
+  if (d != undefined && d.preIco != undefined) {
+    console.log(d.preIco.length);
+
+    preIcoRecords = d.preIco.length;
+    icoRecords = d.ico.length;
+    finalIcoRecords = d.finalIco.length;
+    totalRecords = preIcoRecords + icoRecords + finalIcoRecords;
+     preIcoGrouped = groupBy(d.preIco, cur => cur.currency);
+     icoGrouped = groupBy(d.ico, cur => cur.currency);
+     finalIcoGrouped = groupBy(d.finalIco, cur => cur.currency);
+    console.log(preIcoGrouped);
+    //const key = Array.from(grouped.keys())[0]
+    //console.log(key);
+    //let groupedValues = groupBy(grouped.get(key), pet => pet.value);
+    //console.log(groupedValues);
+    // d.preIco=d.preIco.map((x)=>{  x.valuenew=Math.log10(x.value); return x;});
+    treeData = [
+      {
+        name: 'Data',
+        attributes: {
+          'Total Records': totalRecords
+  
+        },
+        children: [{
+          name: 'PreIco',
+          attributes: {
+            'Records': preIcoRecords
+  
+          },
+          children:[]
+        },
+        {
+          name: 'Ico',
+          attributes: {
+            'Records': icoRecords
+          },
+          children:[]
+        },
+        {
+          name: 'FinalIco',
+          attributes: {
+            'Records': finalIcoRecords
+  
+          },
+          children:[]
+        }
+        ],
+  
+      }
+    ];
+
+    console.log(treeData);
+    for (var [key, value] of preIcoGrouped) {
+      //console.log(key + ' = ' + value);
+      let obj={name:key,attributes:{ 'Records': value.length } };
+      treeData[0].children[0].children.push(obj);
+      
     }
+
+    for (var [key, value] of icoGrouped) {
+      //console.log(key + ' = ' + value);
+      let obj={name:key,attributes:{ 'Records': value.length } };
+      treeData[0].children[1].children.push(obj);
+      
+    }
+
+    for (var [key, value] of finalIcoGrouped) {
+      //console.log(key + ' = ' + value);
+      let obj={name:key,attributes:{ 'Records': value.length } };
+      treeData[0].children[2].children.push(obj);
+      
+    }
+  }
+
+  
+
+  
+  const myTreeData = [
+    {
+      name: 'Top Level',
+      attributes: {
+        keyA: 'val A',
+        keyB: 'val B',
+        keyC: 'val C',
+      },
+      children: [
+        {
+          name: 'Level 2: A',
+          attributes: {
+            keyA: 'val A',
+            keyB: 'val B',
+            keyC: 'val C',
+          },
+        },
+        {
+          name: 'Level 2: B',
+        },
+      ],
+    },
+  ];
+  //console.log(d.preIco);
+  return {
+    data: treeData
+  }
 }
 // function mapStateToProps(state, ownProps) {
 //     return {
@@ -115,11 +240,11 @@ function mapStateToProps(state, ownProps) {
 // }
 
 const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators(apiActions, dispatch)
+  actions: bindActionCreators(apiActions, dispatch)
 }
 )
 
-export const Charts= connect(
-    mapStateToProps,
-    mapDispatchToProps
+export const Charts = connect(
+  mapStateToProps,
+  mapDispatchToProps
 )(ChartsExport)
