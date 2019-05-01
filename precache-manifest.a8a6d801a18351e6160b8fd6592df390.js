209143c111779c79c449d09065b690e1dbe70d48
asifsha <asif-sharif@hotmail.com>
2019-05-01 13:00:36 +0500

    Updates

---
+++ precache-manifest.a8a6d801a18351e6160b8fd6592df390.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c28f0a8229856379658",
+    "url": "/b-data-app/static/js/runtime~main.dde80553.js"
+  },
+  {
+    "revision": "f83c2e396e5f46dacfbe",
+    "url": "/b-data-app/static/js/main.d9e2808d.chunk.js"
+  },
+  {
+    "revision": "9b1de925138ce2a45358",
+    "url": "/b-data-app/static/js/2.7a197ad3.chunk.js"
+  },
+  {
+    "revision": "f83c2e396e5f46dacfbe",
+    "url": "/b-data-app/static/css/main.3590f43a.chunk.css"
+  },
+  {
+    "revision": "8d28757933863b195beb5282de07a1a0",
+    "url": "/b-data-app/index.html"
+  }
+];
